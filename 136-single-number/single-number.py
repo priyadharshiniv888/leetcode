f8531322416b809c44ab8c41dc2f3d102b3971f4
priyadharshiniv888 <priyadharshiniv888@gmail.com>
2026-09-21 14:49:49 +0530

    Time: 7 ms (29.37%) | Memory: 21.1 MB (70.68%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,11 +1,9 @@
 class Solution:
     def singleNumber(self, nums: list[int]) -> int:
-        d={}
+
+        result = 0
+
         for num in nums:
-            if  num not in d:
-                d[num]=1
-            else:
-                d[num]+=1
-        for num in nums:
-            if d[num]==1:
-                return num
+            result = result ^ num
+
+        return result
